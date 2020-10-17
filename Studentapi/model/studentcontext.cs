42489307a5b2e695fd63fb0a0ec24c6416862f01
Murshad 01 <bcs.f17.a.20@gmail.com>
2020-10-17 23:59:04 +0500

    Initial Commit

---
+++ Studentapi/model/studentcontext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentApi.Models
+{
+    public class studentContext : DbContext
+    {
+        public studentContext(DbContextOptions<studentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> TodoItems { get; set; }
+    }
+}
